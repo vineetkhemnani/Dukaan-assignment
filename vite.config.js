b7c39c14e613208696bf8c8272a6eb9a54137caa
vineetkhemnani <vineetkhemnani09@gmail.com>
2024-01-10 00:10:22 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    // get rid of CORS errors
+    proxy: {
+      '/api': {
+        // uncomment below when in local environment
+        target: 'http://localhost:5000',
+        // target: 'https://dhaage-backend.vercel.app/',
+        // target: 'https://threads-backend-2lcn.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
